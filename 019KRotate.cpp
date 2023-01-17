1e939bf52a3e1e326c8eaf70c0f1c35b821da9b9
yashicagarg50 <yashicagarg50@gmail.com>
2023-01-18 03:08:03 +0530

    change

--- 019KRotate.cpp
+++ 019KRotate.cpp
@@ -13,7 +13,49 @@ class ListNode {
     }
 };
 
+void insertAtHead(ListNode*& head, int val) {
+    ListNode* n =new ListNode(val);
+    n->next = head;
+    head = n;
+}
+
+void printLinkedList(ListNode* temp) {
+    while(temp) {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+ListNode* rotateLinkedList(ListNode* head, int k) {
+    // 1. form a loop by connecting the tail ListNode of the linked list with the head ListNode of the LL
+    ListNode* tail = head;
+    int n = 1;
+    while(tail->next != NULL) {
+        tail = tail->next;
+        n++;
+    }
+    tail->next = head;
+
+    // 2. get hold of the pointer to the new tail ListNode that can be found at the (n-(k%n)-1)th index
+
+    ListNode* newTail = head;
+    for(int i=0; i<n-(k%n)-1; i++) {
+        newTail = newTail->next;
+    }
+
+    // 3. get hold of the pointer to the new head ListNode
+
+    ListNode* newHead = newTail->next;
+
+    // 4. remove the cycle by updating the next pointer of the new tail ListNode to contain the NULL address
+    newTail->next = NULL;
+    return newHead;
+}
+
 int main() {
+    ListNode* head = NULL;
+
     insertAtHead(head, 50);
 	insertAtHead(head, 40);
 	insertAtHead(head, 30);
